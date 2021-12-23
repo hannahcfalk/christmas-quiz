bd5f791592e3be19e7d326e32ee138ad6125ca15
Hannah Falk <hannahcfalk@gmail.com>
2021-12-23 13:05:46 +0000

    Add env file

--- chat_room/chat_room/settings.py
+++ chat_room/chat_room/settings.py
@@ -14,13 +14,14 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+config = AutoConfig(search_path=BASE_DIR.joinpath('config'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=ves%10f25+&!@1#yl)bp@_h%bb&tam63a^030f0u0h_v0u%7$'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
